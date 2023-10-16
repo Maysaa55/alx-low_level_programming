9ca3e95b0eaa7e97a71a47d1614f365f24dff054
Maysaa55 <maysaaalimhmd@gmail.com>
2023-10-16 14:12:24 -0700

    file0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - starts of the program and call the function
+ * return : is 0
+ * reset_to_98- update the variable's value to 98
+ * ip - is the parameter of integer pointer type points to te value
+ */
+void reset_to_98(int *ip);
+
+int main(void)
+{
+int num = 40;
+int *ptr = &num;
+reset_to_98(ptr);
+printf("the value of num after resetting : %i\n", num);
+return (0);
+}
+void reset_to_98(int *ip)
+{
+/**
+ * reset_to_98- update the variable's value to 98
+ * ip - is the parameter of integer pointer type points to te value
+ */
+*ip = 98;
+}
