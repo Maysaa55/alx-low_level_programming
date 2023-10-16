69e7a1e801dd00f9b647ccb923980fe2d6c9c2cf
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-16 23:54:02 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,26 +1,3 @@
-//#include <stdio.h>
-//#include "main.h"
-
-/**
- * reset_to_98 - updates the value of the variable
- * the pointer points to to 98
- */
-
-//void reset_to_98(int *ip);
-
-/* int main(void)
-{
-int num = 402;
-printf("n=%d\n", num);
-reset_to_98(&num);
-printf("n=%d\n", num);
-return (0);
-}
-void reset_to_98(int *ip)
-{
-*ip = 98;
-} */
-
 #include "main.h"
 
 /**
