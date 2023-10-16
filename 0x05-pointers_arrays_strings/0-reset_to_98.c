c9ccf4f302849393b4b6731ae149a5331be278b6
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-16 23:48:24 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main - starts of the program and call the function
- * return : is 0
- * reset_to_98- update the variable's value to 98
- * ip - is the parameter of integer pointer type points to te value
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
 void reset_to_98(int *ip);
 
 int main(void)
