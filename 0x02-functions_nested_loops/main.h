6f20c79eda712fead16c1e3792e2028584bf4e2b
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-22 07:35:57 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,74 +1,21 @@
-#ifndef _PUTCHAR_H
-#define _PUTCHAR_H
-int _putchar(char c);
-#endif
 
-#ifndef _STRCAT_H
-#define _STRCAT_H
-char *_strcat(char *dest, char *src);
-#endif
 
-#ifndef _STRNCAT_H
-#define _STRNCAT_H
-char *_strncat(char *dest, char *src, int n);
-#endif
+/**
+*File:main.h
+*Author:Pericles Adjovi
+*Desc:header file containing functions declaration
+*/
 
-#ifndef _STRNCPY_H
-#define _STRNCPY_H
-char *_strncpy(char *dest, char *src, int n);
-#endif
-
-#ifndef _STRNCPY_H
-#define _STRNCPY_H
-char *_strncpy(char *dest, char *src, int n);
-#endif
-
-#ifndef _STRCMP_H
-#define _STRCMP_H
-int _strcmp(char *s1, char *s2);
-#endif
-
-#ifndef _STRCMP_H
-#define _STRCMP_H
-int _strcmp(char *s1, char *s2);
-#endif
-
-#ifndef _REVERSE_ARRAY_H
-#define _REVERSE_ARRAY_H
-void reverse_array(int *a, int n);
-#endif
-
-#ifndef _STRING_TOUPPER_H
-#define _STRING_TOUPPER_H
-char *string_toupper(char *);
-#endif
-
-#ifndef _CAP_STRING_H
-#define _CAP_STRING_H
-char *cap_string(char *);
-#endif
-
-#ifndef LEET_H
-#define LEET_H
-char *leet(char *);
-#endif
-
-#ifndef ROT13_H
-#define ROT13_H
-char *rot13(char *);
-#endif
-
-#ifndef INFINITE_ADD_H
-#define INFINITE_ADD_H
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-#endif
-
-#ifndef PRINT_NUMBER_H
-#define PRINT_NUMBER_H
-void print_number(int n);
-#endif
-
-#ifndef PRINT_BUFFER_H
-#define PRINT_BUFFER_H
-void print_buffer(char *b, int size);
-#endif
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
