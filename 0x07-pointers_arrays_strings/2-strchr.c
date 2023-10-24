d34ba9ff8c625f7de910dcdd606588daaea43e9f
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-24 02:19:20 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,11 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 char *_strchr(char *s, char c)
-{
-int i = 0 ;
-	int j ;
+{int i=0;
+int j;
 while(s[i] != '\0'){
-	i++ ;
-		}
+i++;}
 for(j=0;j<=i;j++,s++){
-	if (*s==c)
-		return(s);
-}
-	return(0);
-}
+if (*s==c)
+return(s);}
+return(0);}
