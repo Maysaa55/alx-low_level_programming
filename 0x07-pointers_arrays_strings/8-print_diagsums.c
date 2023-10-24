e48b530636a2c92b5c4f4227c6c8b5cf61f56415
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-24 03:22:15 +0200

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,9 +15,9 @@ void print_diagsums(int *a, int size)
 {
 int i , j ;
 int sum = 0 ;
-	for (i = 0 ; i < size ; i++)
-		sum += a[i][i] ;
-	for (j = 0 ; j < size ; j++)
-		sum += a[j][size-j-1];
-	printf("%i\n",sum);
+for (i = 0 ; i < size ; i++){
+	sum += a[i][i] ;}
+for (j = 0 ; j < size ; j++){
+	sum += a[j][size-j-1];}
+printf("%i\n",sum);
 }
