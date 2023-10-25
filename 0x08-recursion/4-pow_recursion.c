ff038d867e2dcb9186192e565468facd39f9a028
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-26 01:57:38 +0200

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - raises x to the power of y
+ * @x: Number Integer
+ * @y: Power
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int pow_recursion(int x, int y)
+{
+ if (y > 0)
+ {
+  return pow_recursion(x, y - 1) * x;
+ }
+ if (y == 0)
+  return 1;
+ if (y < 0)
+  return -1;
+}
