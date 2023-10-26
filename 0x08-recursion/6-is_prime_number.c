f7477c274173c81cab808f343d1c8358516b2129
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-26 02:17:34 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@
 int is_prime_number(int n)
 {
  if (n==1)
-  return (1);
+  return (0);
  if ((n != 2 && n % 2 == 0) || (n != 3 && n % 3 == 0) || (n != 5 && n % 5 == 0) || (n != 7 && n % 7 == 0) || (n != 11 && n % 11 == 0))
   return (0);
  else
