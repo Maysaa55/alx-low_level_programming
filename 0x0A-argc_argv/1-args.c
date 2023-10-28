daefb97bf3c93e367e877d49cb64d65fd528b78f
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-28 02:36:35 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc , char** argv)
 {
+  (void)argv;
 printf("%i\n",argc-1);
 //_putchar('\n');
 return 0 ;
