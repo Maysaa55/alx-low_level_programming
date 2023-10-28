ac0c947f7c2309d47a2042832b7a0a7ec51a7e90
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-28 02:28:29 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,15 @@
 #include"main.h"
 #include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc , char* argv[])
 {
 char* str = argv[0];
