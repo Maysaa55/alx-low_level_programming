eba556dac155b365f72c9d4cee208c0f7738e616
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-10-28 02:52:03 +0200

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,11 +10,10 @@
  * Return: Always 0 (Success)
  */
 int main(int argc , char* argv[]){
-  (void)argv;
   int i ;
   
 for ( i = 0 ; i < argc ; i++ ){
-printf("%s\n",argv[i]);
+     printf("%s\n",argv[i]);
 }
- return 0 ; 
+return 0 ; 
 }
