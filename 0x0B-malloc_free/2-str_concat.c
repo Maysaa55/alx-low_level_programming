d16a7a71ab0ecb38fdc4e51a2e966a5da806e245
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-01 01:20:29 +0200

    file2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, k;
+
+	i = 0, j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		j++;
+	if (n < j)
+		j = n - 1;
+	for (k = 0; k <= j; k++, i++)
+		dest[i] = src[k];
+	dest[i + 1] = '\0';
+	return (dest);
+}
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		dest[j] = *(src + j);
+	}
+	return (dest);
+}
+int _strlen(char *s)
+{
+	int length = 0;
+
+		while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+char *str_concat(char *s1, char *s2){
+int length1 = _strlen(s1);
+int length2 = _strlen(s2);
+int length = length1 + length2 ;
+char* newcon = _strcat(s1 , s2 , length);
+char *newstr = malloc(sizeof(char)*(length+1));
+newstr = _strcpy(newstr,newcon);
+return newstr ;
+}
+
