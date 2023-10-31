da0f3a1808c711b36eba7723fc9822981e73cf4c
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-01 00:49:49 +0200

    file1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+char *_strdup(char *str)
+{
+ if (str == NULL)
+  return (0);
+ int i = 0, j = 0;
+ int count = 1;
+ while (*(str + i) != '\0')
+ {
+  count += 1;
+  i++;
+ }
+ char *s = malloc(sizeof(char) * count);
+ for (j = 0; j < count; j++)
+ {
+  *(s + j) = *(str + j);
+ }
+ *(s + count) = '\0';
+ return s;
+}
+
