be66ed724a173d32f07f63df61a31323162c2461
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-01 03:24:02 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,30 @@
 #include"main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * _strdup - creates a copy for given string
+ * @str: the pointer which points to the string
+ *
+ * Return: character pointer points to the new string
+ */
 char *_strdup(char *str)
 {
  if (str == 0)
   return (0);
- int i = 0;
+ int i ;
  int j ;
  int count = 0;
- while (*(str + i) != '\0')
+ while (str[i] != '\0')
  {
   count += 1;
   i++;
  }
  char *s = malloc(sizeof(char) * (count+1));
- for (j = 0; j <= count; j++)
+ for (j = 0; j < count; j++)
  {
-  *(s + j) = *(str + j);
+  s[j] = str[j];
  }
-
+s[count]='\0'
  return s;
 }
 
