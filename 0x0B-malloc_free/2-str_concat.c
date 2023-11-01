a1ab7af0f575bb064bbca7782ae7ce6d1a04fce6
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-01 03:51:23 +0200

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,8 +6,10 @@ char *_strcat(char *dest, char *src)
 	int i, j, k;
 
 	i = 0, j = 0;
+	if(dest[0]!='')
 	while (dest[i] != '\0')
 		i++;
+	if(src[0]!='')
 	while (src[j] != '\0')
 		j++;
 	for (k = 0; k <= j; k++, i++)
