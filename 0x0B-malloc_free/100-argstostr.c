e757fc4143c0920e8cacf0f1e5c81398edc77001
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-01 03:06:56 +0200

    file100edited

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,47 @@
 #include"main.h"
 #include<stdio.h>
 #include<stdlib.h>
-int _strlen(char *s)
-{
-	int length = 0;
 
-		while (s[length] != '\0')
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count in main
+ * @av: arguments passed to main
+ *
+ * Return: Pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int l, lt, i, j, k;
+
+	if (ac == 0 || av == NULL)
+		return (0);
+	l = 0, k = 0;
+	for (i = 0; i < ac; i++)
 	{
-		length++;
+		lt = 0;
+		while (av[i][lt])
+			lt++;
+		l += lt + 1;
 	}
-	return (length);
-}
-char *_strcat(char *dest, char *src)
-{
-	int i, j, k;
+	s = malloc((l + 1) * sizeof(char));
 
-	i = 0, j = 0;
-	if(dest != 0)
-	while (dest[i] != '\n')
-		i++;
-	while (src[j] != '\0')
-		j++;
-	for (k = 0; k <= j; k++, i++)
-		dest[i] = src[k];
-	dest[i+j]='\n';
-	return (dest);
-}
-char *argstostr(int ac, char **av){
-int i ;
-int j ;
-int length = 0 ;
-if (ac == 0 || av == NULL)
- return NULL ;
-for (i = 0 ; i < ac ; i++){
-length += _strlen(*(av+i));
-}
-length += ac ;
-char* newstr = malloc(sizeof(char)*length);
-for ( j = 0 ; j < ac ; j++){
-_strcat( newstr, *(av+j));
-}
-return newstr ;
-}
+	if (s == 0)
+		return (0);
 
+	for (j = 0; j < ac; j++)
+	{
+		lt = 0;
+		while (av[j][lt])
+		{
+			*(s + k) = av[j][lt];
+			k++;
+			lt++;
+		}
+		*(s + k) = '\n';
+		k++;
+	}
+	*(s + k) = '\0';
+
+	return (s);
+}
