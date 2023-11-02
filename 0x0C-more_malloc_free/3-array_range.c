bdc2f4e3c10c88a12609b2293656fcb3e9d22abd
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-03 00:46:40 +0200

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,15 +12,15 @@
 int *array_range(int min, int max)
 {
  if (min > max)
-    return NULL;
+ return NULL;
  int range ;
  int i;
  int *arr ;
  range = max - min + 1;
- arr = (int*)malloc(range);
- arr[0] = min;
+ arr = malloc(sizeof(int)*range);
+*(arr) = min;
  for (i = 1; arr[i] < max; i++)
- arr[i] = min + 1;
+*(arr +i) = min + 1;
  return arr;
  return NULL;
 }
