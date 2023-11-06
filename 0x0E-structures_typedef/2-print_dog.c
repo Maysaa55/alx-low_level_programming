6cf9e3aec5613ad3ca95a127dbc3895d6fcf0417
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-06 23:26:12 +0200

    file2 edited

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,18 +7,15 @@
  * @d: struct pointer points to the printed struct
  **/
 void print_dog(struct dog *d){
-if (d == NULL)
-	exit();
+if (d != NULL){
 char* tmp = "(nil)" ;
 if(d->name == NULL)
  d->name = tmp ;
 printf("Name: %s",d->name);
-if(d->age == NULL)
-printf("Age: %f",d->tmp);
-else 
-	printf("Age: %f",d->age);
+printf("Age: %f",d->age);
 if(d->owner == NULL)
  d->owner = tmp ;
 printf("Owner: %s",d->owner);
+}
 }
 
