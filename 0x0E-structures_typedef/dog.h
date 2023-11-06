f39fdf329e8727c368feb7f1d7ba8a321283bd68
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-06 23:10:33 +0200

    rgkre

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,5 @@
-/**
- * struct dog - Short description
- * @age: the dog's age .
- * @name: the dog's name .
- * @pwner: the owner's name .
- *
- */
-struct dog{
-char* name ;
-float age ;
-char* owner ;
-};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
