a26043d0611691955e8cbfc06c4777b33ee2cf7f
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-06 23:07:58 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,11 +9,14 @@
  * @age:the initialize age of the dog
  * @owner: the initialized name of the dog's owner
  **/
-void init_dog(struct dog *d, char *name, float age, char *owner){
-  if(d != 0 ){
-d->name = name ;
-d->age = age ;
-d->owner = owner ;
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != 0)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
-}
+
 
