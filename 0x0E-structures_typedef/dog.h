4ebbae360ed90e30a35c192dea2471313227e8db
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-06 22:37:36 +0200

    file0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - Short description
+ * @age: the dog's age .
+ * @name: the dog's name .
+ * @pwner: the owner's name .
+ *
+ */
+struct dog{
+char* name ;
+float age ;
+char* owner ;
+};
+
