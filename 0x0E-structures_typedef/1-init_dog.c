c811da3c7c0adb170f0d99ea3c969e7c1031a0f4
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-06 22:56:40 +0200

    file 1 and 2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - initialize a struct dog 
+ * @d: a pointer to a dog struct 
+ * @name: the name of the owner
+ * @age:the initialize age of the dog
+ * @owner: the initialized name of the dog's owner
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner){
+d->name = name ;
+d->age = age ;
+d->owner = owner ;
+}
+
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+/**
+ * print_dog - prints the parameters of the dog struct
+ * @d: struct pointer points to the printed struct
+ **/
+void print_dog(struct dog *d){
+if (d == NULL)
+	exit();
+char* tmp = "(nil)" ;
+if(d->name == NULL)
+ d->name = tmp ;
+printf("Name: %s",d->name);
+if(d->age == NULL)
+ d->age = tmp ;
+printf("Age: %f",d->age);
+if(d->owner == NULL)
+ d->owner = tmp ;
+printf("Owner: %s",d->owner);
+}
+
