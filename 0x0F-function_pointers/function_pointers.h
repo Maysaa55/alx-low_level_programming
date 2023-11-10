abf09978a5345db63c4a541063f6b8f2afa4d773
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-10 02:23:52 +0200

    the header file updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
-int _putchar(char);
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+typedef __SIZE_TYPE__ size_t;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+#endif
 
