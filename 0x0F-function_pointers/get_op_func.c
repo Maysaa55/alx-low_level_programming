80936331d2dc47697f5358351bf77bdbddfb67f9
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-10 03:33:55 +0200

    vnjsdvnj

---
+++ 0x0F-function_pointers/get_op_func.c
@@ -0,0 +1,16 @@
+#include "calc.h"
+
+int (*get_op_func(char *s))(int, int){
+	char oper = {+ , - , * , \ , % } ;
+	int (*funcptr[])(int, int) = { op_add , op_sub , op_mul , op_div , op_mod };
+	int i ;
+	i = 0 ;
+	while(i < 5){
+	if (oper[i] == s)
+	return funcptr[i] ;
+	i** ;
+	}
+	return NULL ;
+
+}
+
