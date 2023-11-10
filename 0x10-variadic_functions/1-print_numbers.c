197404fbd198e6ee4d0341b15ddfa9dc0eca45e6
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-10 06:00:57 +0200

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,8 +4,8 @@
 void print_numbers(const char *separator, const unsigned int n, ...){
 unsigned int i ;
 va_list ptr ;
-va_start(ptr,n);
-for(i = 1 ; i < n ; i++){
+va_start(ptr+1,n);
+for(i = 0 ; i < n ; i++){
 if(separator == NULL)
 printf("%i\n",arg(ptr,int);
 else 
