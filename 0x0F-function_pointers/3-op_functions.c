ef27296473d4d6f4f2fc8cee0fc8c8faa3cb106d
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-10 03:49:44 +0200

    adding documentation to the operators file

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,17 +1,56 @@
 #include "3-calc.h"
+/**
+ * op_add - Adds two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Sum of a and b
+ */
 
 int op_add(int a, int b){
 	return (a+b) ;
 }
+/**
+ * op_sub - subtracts two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Sub of a and b
+ */
+
 int op_sub(int a, int b){
 	return (a-b) ;
 }
+/**
+ * op_mul - multiplies two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: multiply of a and b
+ */
+
 int op_mul(int a, int b){
 	return (a*b) ;
 }
+/**
+ * op_div - divides two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: division of a and b
+ */
+
 int op_div(int a, int b){
 	return (a/b) ;
 }
+/**
+ * op_mod - gives the module of two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: modulo of a and b
+ */
+
 int op_mod(int a, int b){
 	return (a%b) ;
 }
