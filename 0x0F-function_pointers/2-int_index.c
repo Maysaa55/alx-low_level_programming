99aaecb81dc12b70d86c95a6b57405920ceb3993
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-10 02:54:10 +0200

    file2 edited

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,18 +10,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int)){
 	
-	 if (array != 0 && size > 0){
+	 if (array != 0 && size > 0 ){
 	        int i ;
 		for(i = 0 ; i < size ; i++){
-		
-		if(cmp(array[i] != 0)
+		if(cmp(array[i])
 			return i ;
-
 		}
-	
-
 	}
 	return -1 ;
-
 }
 
