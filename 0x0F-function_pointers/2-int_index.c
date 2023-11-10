6d2ee352ab57c98d63331abf897dbebb77bb35d7
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-10 02:59:41 +0200

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int)){
 	 if (array && cmp && size > 0 ){
 	      
 		for(i = 0 ; i < size ; i++){
-		if(cmp(array[i])
-			return (i) ;
+		if(cmp(array[i]))
+			return i ;
 		}
 	}
-	return (-1) ;
+	return -1 ;
 }
 
