90d2214edd39d32b1750c218f9f0a6e40f543764
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-17 02:11:04 +0200

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * printbefore - prints a message 
+ *
+ * Return - no thing
+ */
+void printbefore(){
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+}
+
