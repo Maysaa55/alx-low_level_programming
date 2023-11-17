1d895925ed59a8cf78e7353a9c5bc24d44b2516f
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-17 02:15:07 +0200

    kvd

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
+
+void msg(void)__attribute__((constructor));
+/**
  * printbefore - prints a message 
  *
  * Return - no thing
  */
 void printbefore(){
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-
 }
 
