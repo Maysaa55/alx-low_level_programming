d4ee5a105a81607e2c180aea55fb21e8d87a6d32
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-21 01:01:08 +0200

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * free_listint - print a list of integers
+ * @head: the header pointer to the list
+ *
+ * Return - nothing
+ */
 void free_listint(listint_t *head)
 {
 	if (head == 0)
