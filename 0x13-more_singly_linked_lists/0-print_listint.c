c2f51f29c99694968c0b55b99d2153cb92b74d48
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-20 23:15:37 +0200

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,13 @@
 #include"lists.h"
+#include<stdio.h>
+#include<stddef.h>
+
+/**
+ * print_listint - print a list of integers
+ * @h: the header pointer to the list
+ *
+ * Return - the number of nodes in the list
+ */
 
 size_t print_listint(const listint_t *h)
 {
