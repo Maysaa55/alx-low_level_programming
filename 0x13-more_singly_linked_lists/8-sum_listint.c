a0f763448aa0dfe67371c83a6e024d4ed9f03066
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-21 02:03:02 +0200

    documentation added

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,15 @@
 #include "lists.h"
 
+/**
+ * sum_listint - sums the elements of the list
+ * @head: the list's head pointer
+ *
+ * Return - the sum of all the elements
+ */
 int sum_listint(listint_t *head)
 {
 	int counter;
+
 	counter = 0;
 	while(head != 0)
 	{
