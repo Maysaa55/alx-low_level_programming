9bd472da99f730c6b614abcc0a0f5eec3fae1fcc
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-21 12:34:01 +0200

    mrkre

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < idx-1; i++)
 	{i
 		if (*head == 0)
+		{
+			temp->n = n;
+			temp->next = 0;
+			*head = temp;
 			return (0);
+
 		*head = *head->next;
 	}
 	temp->n = n;
