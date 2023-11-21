e804caa13ec9978c7d571e4e8dc82a56517581c3
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-21 12:29:25 +0200

    kmgf

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,13 +10,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (0);
 	for (i = 0; i < idx-1; i++)
 	{i
-		if (head == 0)
+		if (*head == 0)
 			return (0);
-		head = head->next;
+		*head = *head->next;
 	}
 	temp->n = n;
-	temp->next = head->next;
-	head->next = temp;
+	temp->next = *head->next;
+	*head->next = temp;
 	return (temp);
 }
 
