e04e0ad12127441e7da74e4e62d39a5faa20605e
Maysaa55 <maysaaalimhmd@gmail.com>
2023-11-28 00:07:56 +0200

    file0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * ssize_t read_textfile - reads the text file and print it 
+ * @filename: points to the file's name
+ * @letters: the number of letters to print 
+ *
+ * Return - the number of the letters it could read and print 
+ */
+ssize_t read_textfile(const char *filename, size_t letters){
+FILE *fp ;
+char c ;
+int counter ;
+counter = 0 ;
+if (filename == 0)
+	return (0);
+else 
+{
+	fp = fopen (filename, "r");
+	if (fp == 0)
+		return (0);
+	else 
+	{
+		while ((c = fgetc(fp)) != EOF && counter >= letters){
+			_putchar(c);
+			counter++; 
+		}
+		fclose(fp);
+	}
+	return (counter);
+}
+}
+
