2a4baf8d2e5fe5e4de2f3f1916ce139874da9451
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-11-28 01:49:18 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * read_textfile - reads the text file and print it
- * @filename: points to the file's name
- * @letters: the number of letters to print
+ * read_textfile -  reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
  *
- * Return - the number of the letters it could read and print
+ * Return: returns the actual number of letters it could read and print
+ * 0 if file cannot be opened or read
+ * 0 if filename is NULL
+ * 0 if write fails or does not write expected amount of bytes
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 FILE *fp;
