03f77e65e2dc873d7917367e06350131a6533c7f
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2023-12-15 04:58:49 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,30 +10,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  if (b == 0)
-    return (0);
-  else
-  {
-    int sum, len, counter, i;
+if (b == 0)
+return (0);
+else
+{
+int sum, len, counter, i;
 
-    sum = 0; len = 0; counter = 1;
-    while (b[len] != '\0')
-     len++;
-    if (b[len - 1] == '1')
-     sum = 1;
-    for (i = len - 2; i >= 0; i--)
- {
-     if (b[i] == '1')
-  {
-    sum += (2 * counter);
-    counter *= 2;
-  }
-    else if (b[i] == '0')
-     counter *= 2;
-    else
-     return (0);
- }
-    return (sum);
-  }
-  return (0);
+sum = 0; len = 0; counter = 1;
+while (b[len] != '\0')
+len++;
+if (b[len - 1] == '1')
+sum = 1;
+for (i = len - 2; i >= 0; i--)
+{
+if (b[i] == '1')
+{
+sum += (2 * counter);
+counter *= 2;
+}
+else if (b[i] == '0')
+counter *= 2;
+else
+return (0);
+}
+return (sum);
+}
+return (0);
 }
